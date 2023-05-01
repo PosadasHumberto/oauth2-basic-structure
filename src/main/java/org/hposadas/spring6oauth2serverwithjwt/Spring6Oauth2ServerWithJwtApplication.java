0e9dfb2101416814dde2366b273a1aac46dedb8e
Humberto Posadas <posadas.humberto@outlook.com>
2023-05-01 14:22:34 +0200

    Creating structure project & SecurityConfig

---
+++ src/main/java/org/hposadas/spring6oauth2serverwithjwt/Spring6Oauth2ServerWithJwtApplication.java
@@ -0,0 +1,13 @@
+package org.hposadas.spring6oauth2serverwithjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6Oauth2ServerWithJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6Oauth2ServerWithJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hposadas/spring6oauth2serverwithjwt/Spring6Oauth2ServerWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hposadas.spring6oauth2serverwithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6Oauth2ServerWithJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
